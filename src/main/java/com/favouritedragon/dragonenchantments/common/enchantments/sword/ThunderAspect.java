978c1cb1e067b8f3bf1b0abe52dec301d51b1ef2
FavouriteDragon <ninjadragon008@icloud.com>
2019-06-16 14:03:01 -0700

    ok thunder aspect should work now

--- src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/sword/ThunderAspect.java
+++ src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/sword/ThunderAspect.java
@@ -7,6 +7,7 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
@@ -35,17 +36,33 @@ public class ThunderAspect extends Enchantment {
 		if (attacker instanceof EntityLivingBase) {
 			ItemStack stack = ((EntityLivingBase) attacker).getHeldItemMainhand();
 			if (stack.isItemEnchanted()) {
-				if (attacker instanceof EntityPlayer && !((EntityPlayer) attacker).getCooldownTracker().hasCooldown(stack.getItem())) {
-					int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.thunderAspect, stack);
-					if (level > 0 && hurt != null) {
-						hurt.attackEntityFrom(DamageSource.LIGHTNING_BOLT, event.getAmount() / 10 * level * 2);
-						Vec3d lookVec = attacker.getLookVec();
-						hurt.motionX += lookVec.x * (1 + 0.2 * level);
-						hurt.motionY += lookVec.y * (1 + 0.2 * level);
-						hurt.motionZ += lookVec.z * (1 + 0.2 * level);
-						if (attacker.world.isRemote) {
-							attacker.world.playSound(hurt.posX, hurt.posY, hurt.posZ, SoundEvents.ENTITY_LIGHTNING_IMPACT, SoundCategory.PLAYERS,
-									1.0F + attacker.world.rand.nextFloat(), 1.0F + attacker.world.rand.nextFloat(), true);
+				if (attacker instanceof EntityPlayer) {
+					//This requires some weird stuff for a sweep attack (wtf, mojang)
+					float modifier = EnchantmentHelper.getModifierForCreature(stack, EnumCreatureAttribute.UNDEFINED);
+					float mult = ((EntityPlayer) attacker).getCooledAttackStrength(0.5F);
+					modifier *= mult;
+					if (modifier > 0.0F) {
+						int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.thunderAspect, stack);
+						if (level > 0 && hurt != null) {
+							hurt.attackEntityFrom(DamageSource.LIGHTNING_BOLT, event.getAmount() / 10 * level * 2);
+							Vec3d lookVec = attacker.getLookVec();
+							hurt.motionX += lookVec.x * (1 + 0.2 * level);
+							hurt.motionY += lookVec.y * (1 + 0.2 * level);
+							hurt.motionZ += lookVec.z * (1 + 0.2 * level);
+							if (attacker.world.isRemote) {
+								attacker.world.playSound(hurt.posX, hurt.posY, hurt.posZ, SoundEvents.ENTITY_LIGHTNING_IMPACT, SoundCategory.PLAYERS,
+										1.0F + attacker.world.rand.nextFloat(), 1.0F + attacker.world.rand.nextFloat(), true);
+							}
+						}
+					}
+					else {
+						int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.thunderAspect, stack);
+						if (level > 0 && hurt != null) {
+							hurt.attackEntityFrom(DamageSource.LIGHTNING_BOLT,event.getAmount() / 10 * level);
+							Vec3d lookVec = attacker.getLookVec();
+							hurt.motionX += lookVec.x * (1 + 0.15 * level);
+							hurt.motionY += lookVec.y * (1 + 0.15 * level);
+							hurt.motionZ += lookVec.z * (1 + 0.15 * level);
 						}
 					}
 				}
--- src/main/java/com/favouritedragon/dragonenchantments/common/util/DragonUtils.java
+++ src/main/java/com/favouritedragon/dragonenchantments/common/util/DragonUtils.java
@@ -1,22 +1,20 @@
 package com.favouritedragon.dragonenchantments.common.util;
 
 import com.favouritedragon.dragonenchantments.DragonEnchants;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.util.DamageSource;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 @Mod.EventBusSubscriber(modid = DragonEnchants.MODID)
 public class DragonUtils {
 	private static final DataParameter<Boolean> POWERED;
 
 	static {
-		POWERED = ObfuscationReflectionHelper.getPrivateValue(EntityCreeper.class, null, "POWERED", "field_184714_b");
+		POWERED = ReflectionHelper.getPrivateValue(EntityCreeper.class, null, "POWERED", "field_184714_b");
 	}
 
 	public static void chargeCreeper(EntityCreeper creeper) {
