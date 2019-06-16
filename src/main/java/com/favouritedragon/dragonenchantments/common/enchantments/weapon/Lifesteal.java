bd812784c6e376760d85720dfc0574e934a0be23
FavouriteDragon <ninjadragon008@icloud.com>
2019-06-15 21:46:23 -0700

    halp

--- src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/weapon/Lifesteal.java
+++ src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/weapon/Lifesteal.java
@@ -7,6 +7,7 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -23,17 +24,24 @@ public class Lifesteal extends Enchantment {
 	public static void onLifeSteal(LivingHurtEvent event) {
 		Entity attacker = event.getSource().getTrueSource();
 		if (attacker instanceof EntityLivingBase && !attacker.world.isRemote){
-			int level = EnchantmentHelper.getMaxEnchantmentLevel(ModEnchantments.lifeSteal, ((EntityLivingBase) attacker));
+			ItemStack stack = null;
+			if (((EntityLivingBase) attacker).getHeldItemMainhand().isItemEnchanted()) {
+				stack = ((EntityLivingBase) attacker).getHeldItemMainhand();
+			}
+			else if (((EntityLivingBase) attacker).getHeldItemOffhand().isItemEnchanted()) {
+				stack = ((EntityLivingBase) attacker).getHeldItemOffhand();
+			}
+			int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.lifeSteal, stack);
 			if (level > 0) {
 				((EntityLivingBase) attacker).heal(event.getAmount() / 10 * level * level);
 			}
 		}
 	}
 
-	/*@Override
+	@Override
 	public boolean isTreasureEnchantment() {
 		return true;
-	}**/
+	}
 
 	@Override
 	public int getMaxLevel() {
@@ -42,7 +50,7 @@ public class Lifesteal extends Enchantment {
 
 	@Override
 	public int getMinEnchantability(int enchantmentLevel) {
-		return 10 + 20 * (enchantmentLevel - 1);
+		return 10 + 20 * (enchantmentLevel);
 	}
 
 	@Override
