b5e6ca7bba73adb1b7a9e4ebeecb82fcc815352e
FavouriteDragon <ninjadragon008@icloud.com>
2019-06-16 09:59:47 -0700

    dragon slayer will now work correctly

--- src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/weapon/DragonSlayer.java
+++ src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/weapon/DragonSlayer.java
@@ -7,7 +7,10 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -23,9 +26,26 @@ public class DragonSlayer extends Enchantment {
 	@SubscribeEvent
 	public static void onDragonHurt(LivingHurtEvent event) {
 		Entity attacker = event.getSource().getTrueSource();
-		if (attacker instanceof EntityLivingBase && !attacker.world.isRemote && (((EntityLivingBase) attacker).getHeldItemMainhand().isItemEnchanted() ||
-				((EntityLivingBase) attacker).getHeldItemOffhand().isItemEnchanted())) {
-			int level = EnchantmentHelper.getMaxEnchantmentLevel(ModEnchantments.dragonSlayer, ((EntityLivingBase) attacker));
+		if (attacker instanceof EntityLivingBase && !attacker.world.isRemote) {
+			ItemStack stack = null;
+			int level;
+			ItemStack mainStack = ((EntityLivingBase) attacker).getHeldItemMainhand();
+			ItemStack offStack = ((EntityLivingBase) attacker).getHeldItemOffhand();
+			if (event.getSource().getImmediateSource() instanceof EntityArrow) {
+				if (mainStack.isItemEnchanted() && mainStack.getItem() == Items.BOW) {
+					stack = mainStack;
+				} else if (offStack.isItemEnchanted() && offStack.getItem() == Items.BOW) {
+					stack = offStack;
+				}
+			} else {
+				if (mainStack.isItemEnchanted()) {
+					stack = mainStack;
+				} else if (offStack.isItemEnchanted()) {
+					stack = offStack;
+				}
+			}
+			if (stack == null) return;
+			level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.lifeSteal, stack);
 			if (level > 0 && event.getEntityLiving() instanceof EntityDragon) {
 				event.setAmount(event.getAmount() * (1 + 0.1F * level));
 			}
