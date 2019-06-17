4e3788b6d5ab5c81404867bf81493d591fbf1e58
FavouriteDragon <ninjadragon008@icloud.com>
2019-06-16 20:47:15 -0700

    new enchant

---
+++ src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/sword/SoulDevour.java
@@ -0,0 +1,13 @@
+package com.favouritedragon.dragonenchantments.common.enchantments.sword;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.inventory.EntityEquipmentSlot;
+
+public class SoulDevour extends Enchantment {
+
+	protected SoulDevour(Rarity rarityIn, EnumEnchantmentType typeIn, EntityEquipmentSlot[] slots) {
+		super(rarityIn, typeIn, slots);
+	}
+}
