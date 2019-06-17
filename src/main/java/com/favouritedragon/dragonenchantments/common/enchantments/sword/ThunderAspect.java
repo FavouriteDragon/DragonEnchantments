f74ff00afd31ac6ef5e10dae9fc9fd8a7b223ebc
FavouriteDragon <ninjadragon008@icloud.com>
2019-06-16 20:36:31 -0700

    tried to get crits working; failed

--- src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/sword/ThunderAspect.java
+++ src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/sword/ThunderAspect.java
@@ -18,13 +18,13 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.lwjgl.Sys;
 
-@Mod.EventBusSubscriber(modid = DragonEnchants.MODID)
+//@Mod.EventBusSubscriber(modid = DragonEnchants.MODID)
 public class ThunderAspect extends Enchantment {
+
+	private float sweepMult = 0;
 	public ThunderAspect() {
 		super(Rarity.RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND});
 		setRegistryName("thunder_aspect");
@@ -32,7 +32,14 @@ public class ThunderAspect extends Enchantment {
 	}
 
 	@SubscribeEvent
-	public static void onSwingEvent(LivingAttackEvent event) {
+	public void onCrit(CriticalHitEvent event) {
+		if (event.getEntity() instanceof EntityPlayer && event.getTarget() instanceof EntityLivingBase) {
+			setSweepMult(getSweepAttackMult() * event.getDamageModifier());
+		}
+	}
+
+	@SubscribeEvent
+	public void onSwingEvent(LivingAttackEvent event) {
 		Entity attacker = event.getSource().getTrueSource();
 		EntityLivingBase hurt = event.getEntityLiving();
 		if (attacker instanceof EntityLivingBase) {
@@ -40,24 +47,15 @@ public class ThunderAspect extends Enchantment {
 			if (stack.isItemEnchanted()) {
 				if (attacker instanceof EntityPlayer) {
 					//This requires some weird stuff for a sweep attack (wtf, mojang)
-					float attackMult = (float) ((EntityPlayer) attacker).getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
+					setInitialSweepMult((EntityPlayer) attacker);
+					//float attackMult = (float) ((EntityPlayer) attacker).getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
 					float modifier = EnchantmentHelper.getModifierForCreature(stack, EnumCreatureAttribute.UNDEFINED);
 					float mult = ((EntityPlayer) attacker).getCooledAttackStrength(0.5F);
-					boolean doCrit = mult > 0.9F;
 					modifier *= mult;
-					attackMult = attackMult * (0.2F + mult * mult * 0.8F);
-					attackMult += modifier;
-					boolean crit = doCrit && attacker.fallDistance > 0.0F && !attacker.onGround && !((EntityPlayer) attacker).isOnLadder() &&
-							!attacker.isInWater() && !(((EntityPlayer) attacker).isPotionActive(MobEffects.BLINDNESS) && !attacker.isRiding() &&
-							!attacker.isSprinting() && hurt instanceof EntityLivingBase);
-
-					net.minecraftforge.event.entity.player.CriticalHitEvent hitResult =
-							net.minecraftforge.common.ForgeHooks.getCriticalHit((EntityPlayer) attacker, hurt, crit, crit ? 1.5F : 1.0F);
-					if (hitResult != null) {
-						attackMult *= hitResult.getDamageModifier();
-					}
+					setSweepMult(getSweepAttackMult() * (0.2F + mult * mult * 0.8F));
+					setSweepMult(getSweepAttackMult() +  modifier);
 					//Checks to see if the damage is equivalent to a sweeping attack
-					if (event.getAmount() == (1.0F + EnchantmentHelper.getSweepingDamageRatio((EntityLivingBase) attacker) * attackMult)) {
+					if (event.getAmount() == (1.0F + EnchantmentHelper.getSweepingDamageRatio((EntityLivingBase) attacker) * getSweepAttackMult())) {
 						int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.thunderAspect, stack);
 						if (level > 0 && hurt != null) {
 							hurt.attackEntityFrom(DamageSource.LIGHTNING_BOLT, event.getAmount() / 10 * level * 2);
@@ -115,6 +113,18 @@ public class ThunderAspect extends Enchantment {
 	@Override
 	public boolean isTreasureEnchantment() {
 		return true;
+	}
+
+	private float getSweepAttackMult() {
+		return sweepMult;
+	}
+
+	private void setInitialSweepMult(EntityPlayer player) {
+		this.sweepMult = (float) player.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
+	}
+	//Used in the crit method
+	private void setSweepMult(float mult) {
+		this.sweepMult = mult;
 	}
 
 }
