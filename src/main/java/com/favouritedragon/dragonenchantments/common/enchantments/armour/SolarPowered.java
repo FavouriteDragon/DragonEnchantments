005314c09d88a07b67a3a157e120a0d1d1ab7b9d
FavouriteDragon <ninjadragon008@icloud.com>
2019-06-18 10:55:16 -0700

    hopefully finished solar powered's effect

--- src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/armour/SolarPowered.java
+++ src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/armour/SolarPowered.java
@@ -1,11 +1,21 @@
 package com.favouritedragon.dragonenchantments.common.enchantments.armour;
 
 import com.favouritedragon.dragonenchantments.DragonEnchants;
+import com.favouritedragon.dragonenchantments.common.enchantments.ModEnchantments;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
 
 public class SolarPowered extends Enchantment {
 	//Restores health and durability
@@ -18,7 +28,27 @@ public class SolarPowered extends Enchantment {
 
 	@SubscribeEvent
 	public static void onSun(LivingEvent.LivingUpdateEvent event) {
+		EntityLivingBase entity = event.getEntityLiving();
+		if (entity != null) {
+			if (entity.world.isDaytime()) {
+				if(entity.world.canSeeSky(entity.getPosition())) {
+					int level = EnchantmentHelper.getMaxEnchantmentLevel(ModEnchantments.solarPowered, entity);
+					if (level > 0) {
+						if (!entity.isPotionActive(MobEffects.REGENERATION)) {
+							entity.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 20, level - 1, false, false));
+						}
+						Iterable<ItemStack> armour = entity.getEquipmentAndArmor();
+						for (ItemStack stack : armour) {
+							int armourLevel = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.solarPowered, stack);
+							if (entity.ticksExisted % 400 - level * 20 == 0) {
+								stack.damageItem(-armourLevel, entity);
+							}
 
+						}
+					}
+				}
+			}
+		}
 	}
 
 
