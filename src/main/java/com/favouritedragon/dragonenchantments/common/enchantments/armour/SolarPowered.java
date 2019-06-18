3aba1dcaa4ff2aa18e6dc9e6aa6a8fb05c11eafd
FavouriteDragon <ninjadragon008@icloud.com>
2019-06-17 21:54:38 -0700

    new enchantment

---
+++ src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/armour/SolarPowered.java
@@ -0,0 +1,12 @@
+package com.favouritedragon.dragonenchantments.common.enchantments.armour;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.inventory.EntityEquipmentSlot;
+
+public class SolarPowered extends Enchantment {
+	//Restores health and durability
+	public SolarPowered(Rarity rarityIn, EnumEnchantmentType typeIn, EntityEquipmentSlot[] slots) {
+		super(rarityIn, typeIn, slots);
+	}
+}
