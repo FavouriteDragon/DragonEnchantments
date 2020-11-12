b2cef005d3b1aa292c73d1431c1e24d3551722b8
John <ninjadragon008@icloud.com>
2020-11-11 16:01:13 -0800

    oop minor fixes

--- src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/weapon/sword/ThunderAspect.java
+++ src/main/java/com/favouritedragon/dragonenchantments/common/enchantments/weapon/sword/ThunderAspect.java
@@ -27,124 +27,118 @@ import java.util.HashMap;
 @Mod.EventBusSubscriber(modid = DragonEnchants.MODID)
 public class ThunderAspect extends Enchantment {
 
-	private static HashMap<String, Boolean> isSweepAttack = new HashMap<>();
+    private static final HashMap<String, Boolean> isSweepAttack = new HashMap<>();
 
-	public ThunderAspect() {
-		super(Rarity.RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND});
-		setRegistryName("thunder_aspect");
-		setName(DragonEnchants.MODID + ":" + "thunder_aspect");
-	}
+    public ThunderAspect() {
+        super(Rarity.RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND});
+        setRegistryName("thunder_aspect");
+        setName(DragonEnchants.MODID + ":" + "thunder_aspect");
+    }
 
 
-	//Ensures isSweepAttack is never null
-	@SubscribeEvent
-	public static void onEnchantmentGet(LivingEvent.LivingUpdateEvent event) {
-		if (event.getEntityLiving() != null) {
-			EntityLivingBase entity = event.getEntityLiving();
-			int level = DragonUtils.getHeldLevelForEnchantment(entity, ModEnchantments.thunderAspect);
-			if (level > 0) {
-				if (!isSweepAttack.containsKey(entity.getUniqueID().toString())) {
-					setIsSweepAttack(entity.getUniqueID().toString(), false);
-				}
-			}
-		}
-	}
+    //Ensures isSweepAttack is never null
+    @SubscribeEvent
+    public static void onEnchantmentGet(LivingEvent.LivingUpdateEvent event) {
+        if (event.getEntityLiving() != null) {
+            EntityLivingBase entity = event.getEntityLiving();
+            int level = DragonUtils.getHeldLevelForEnchantment(entity, ModEnchantments.thunderAspect);
+            if (level > 0) {
+                if (!isSweepAttack.containsKey(entity.getUniqueID().toString())) {
+                    setIsSweepAttack(entity.getUniqueID().toString(), false);
+                }
+            }
+        }
+    }
 
-	@SubscribeEvent
-	public static void onPlayerSweep(AttackEntityEvent event) {
-		EntityPlayer player = event.getEntityPlayer();
-		if (player != null) {
-			if (player.getCooledAttackStrength(0) >= 1.0) {
-				setIsSweepAttack(player.getUniqueID().toString(), true);
-			} else {
-				setIsSweepAttack(player.getUniqueID().toString(), false);
-			}
-		}
-	}
+    @SubscribeEvent
+    public static void onPlayerSweep(AttackEntityEvent event) {
+        EntityPlayer player = event.getEntityPlayer();
+        if (player != null) {
+            setIsSweepAttack(player.getUniqueID().toString(), player.getCooledAttackStrength(0) >= 1.0);
+        }
+    }
 
-	@SubscribeEvent
-	public static void onSwingEvent(LivingAttackEvent event) {
-		Entity attacker = event.getSource().getTrueSource();
-		EntityLivingBase hurt = event.getEntityLiving();
-		if (attacker instanceof EntityLivingBase) {
-			ItemStack stack = ((EntityLivingBase) attacker).getHeldItemMainhand();
-			if (hurt != null && hurt != attacker && stack.isItemEnchanted() && !(event.getSource() instanceof EntityDamageSourceIndirect)) {
-				if (getSweepAttack(attacker.getUniqueID().toString())) {
-					int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.thunderAspect, stack);
-						if (level > 0) {
-							if (!attacker.world.isRemote) {
-								float amount = level;
-								if (attacker.world.isRaining()) {
-									amount *= 1.25F;
-								} else if (attacker.world.isThundering()) {
-									amount *= 1.5f;
-								}
-								hurt.attackEntityFrom(DamageSource.LIGHTNING_BOLT, event.getAmount() / 5 * amount);
-								Vec3d lookVec = attacker.getLookVec();
-								hurt.motionX += lookVec.x * (1 + 0.2 * amount);
-								hurt.motionY += lookVec.y > 0 ? lookVec.y * (1 + 0.2 * amount) : 0.2 * amount;
-								hurt.motionZ += lookVec.z * (1 + 0.2 * amount);
-							}
-							if (attacker.world.isRemote) {
-								attacker.world.playSound(hurt.posX, hurt.posY, hurt.posZ, SoundEvents.ENTITY_LIGHTNING_IMPACT, SoundCategory.PLAYERS,
-										1.0F + attacker.world.rand.nextFloat(), 1.0F + attacker.world.rand.nextFloat(), true);
-							}
-						}
-					setIsSweepAttack(attacker.getUniqueID().toString(), false);
+    @SubscribeEvent
+    public static void onSwingEvent(LivingAttackEvent event) {
+        Entity attacker = event.getSource().getTrueSource();
+        EntityLivingBase hurt = event.getEntityLiving();
+        if (attacker instanceof EntityLivingBase) {
+            ItemStack stack = ((EntityLivingBase) attacker).getHeldItemMainhand();
+            if (hurt != null && hurt != attacker && stack.isItemEnchanted() && !(event.getSource() instanceof EntityDamageSourceIndirect)) {
+                if (getSweepAttack(attacker.getUniqueID().toString())) {
+                    int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.thunderAspect, stack);
+                    if (level > 0) {
+                        if (!attacker.world.isRemote) {
+                            float amount = level;
+                            if (attacker.world.isRaining()) {
+                                amount *= 1.25F;
+                            } else if (attacker.world.isThundering()) {
+                                amount *= 1.5f;
+                            }
+                            hurt.attackEntityFrom(DamageSource.LIGHTNING_BOLT, event.getAmount() / 5 * amount);
+                            Vec3d lookVec = attacker.getLookVec();
+                            hurt.addVelocity(lookVec.x * (1 + 0.2 * amount), lookVec.y > 0 ? lookVec.y * (1 + 0.2 * amount) : 0.2 * amount,
+                                    lookVec.z * (1 + 0.2 * amount));
+                        }
+                        if (attacker.world.isRemote) {
+                            attacker.world.playSound(hurt.posX, hurt.posY, hurt.posZ, SoundEvents.ENTITY_LIGHTNING_IMPACT, SoundCategory.PLAYERS,
+                                    1.0F + attacker.world.rand.nextFloat(), 1.0F + attacker.world.rand.nextFloat(), true);
+                        }
+                    }
+                    setIsSweepAttack(attacker.getUniqueID().toString(), false);
 
-				} else {
-					int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.thunderAspect, stack);
-					if (level > 0) {
-						if (!attacker.world.isRemote) {
-							float amount = level;
-							if (attacker.world.isRaining()) {
-								amount *= 1.25F;
-							} else if (attacker.world.isThundering()) {
-								amount *= 1.5f;
-							}
-							hurt.attackEntityFrom(DamageSource.LIGHTNING_BOLT, event.getAmount() / 10 * amount);
-							Vec3d lookVec = attacker.getLookVec();
-							hurt.motionX += lookVec.x * (1 + 0.1 * amount);
-							hurt.motionY += lookVec.y > 0 ? lookVec.y * (1 + 0.1 * amount) : 0.1 * amount;
-							hurt.motionZ += lookVec.z * (1 + 0.1 * amount);
-						}
-					}
-				}
+                } else {
+                    int level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.thunderAspect, stack);
+                    if (level > 0) {
+                        if (!attacker.world.isRemote) {
+                            float amount = level;
+                            if (attacker.world.isRaining()) {
+                                amount *= 1.25F;
+                            } else if (attacker.world.isThundering()) {
+                                amount *= 1.5f;
+                            }
+                            hurt.attackEntityFrom(DamageSource.LIGHTNING_BOLT, event.getAmount() / 10 * amount);
+                            Vec3d lookVec = attacker.getLookVec();
+                            hurt.addVelocity(lookVec.x * (1 + 0.1 * amount), lookVec.y > 0 ? lookVec.y * (1 + 0.1 * amount) : 0.1 * amount,
+                                    lookVec.z * (1 + 0.1 * amount));
+                        }
+                    }
+                }
 
-			}
-		}
+            }
+        }
 
-	}
+    }
 
-	private static void setIsSweepAttack(String UUID, boolean sweepAttack) {
-		if (isSweepAttack.containsKey(UUID)) {
-			isSweepAttack.replace(UUID, sweepAttack);
-		} else {
-			isSweepAttack.put(UUID, sweepAttack);
-		}
-	}
+    private static void setIsSweepAttack(String UUID, boolean sweepAttack) {
+        if (isSweepAttack.containsKey(UUID)) {
+            isSweepAttack.replace(UUID, sweepAttack);
+        } else {
+            isSweepAttack.put(UUID, sweepAttack);
+        }
+    }
 
-	private static boolean getSweepAttack(String UUID) {
-		return isSweepAttack.get(UUID) == null ? false : isSweepAttack.get(UUID);
-	}
+    private static boolean getSweepAttack(String UUID) {
+        return isSweepAttack.get(UUID) != null && isSweepAttack.get(UUID);
+    }
 
-	@Override
-	public int getMaxLevel() {
-		return 3;
-	}
+    @Override
+    public int getMaxLevel() {
+        return 3;
+    }
 
-	@Override
-	public int getMinEnchantability(int enchantmentLevel) {
-		return 1 + enchantmentLevel * 80;
-	}
+    @Override
+    public int getMinEnchantability(int enchantmentLevel) {
+        return 1 + enchantmentLevel * 80;
+    }
 
-	@Override
-	public int getMaxEnchantability(int enchantmentLevel) {
-		return getMinEnchantability(enchantmentLevel) + 200;
-	}
+    @Override
+    public int getMaxEnchantability(int enchantmentLevel) {
+        return getMinEnchantability(enchantmentLevel) + 200;
+    }
 
-	@Override
-	public boolean isTreasureEnchantment() {
-		return true;
-	}
+    @Override
+    public boolean isTreasureEnchantment() {
+        return true;
+    }
 }
